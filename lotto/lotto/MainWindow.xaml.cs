5299e26c8b476609041e2a5f42a6de2a3f996a3a
Laurens Pergola <12102886@student.pxl.be>
2024-11-17 19:54:39 +0100

    afgewerkt laurens

---
+++ lotto/lotto/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lotto
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+           StringBuilder sb = new StringBuilder();
+           Random numberGen = new Random();
+
+            for (int i = 1; i < 7; i++)
+            {
+                int randomNr = numberGen.Next(1, 46);
+                string result = $"Getal{i}: {randomNr}";
+                sb.AppendLine(result);
+
+            }
+            resultTextBlock.Text = sb.ToString();
+            
+        }
+
+        private void eraseButton_Click(object sender, RoutedEventArgs e)
+        {
+            resultTextBlock.Text =  string.Empty;
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
